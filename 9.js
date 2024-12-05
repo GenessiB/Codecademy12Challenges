185a55a0b847d1c984a40e114d71a39595f01b17
Genessi Barot <genessibarot@gmail.com>
2024-12-05 17:34:18 -0500

    problem 9 solved

---
+++ 9.js
@@ -0,0 +1,17 @@
+// 9. Return a Boolean if a number is divisible by 10
+/**
+ * input number
+ * output true or false
+ * if divisible by 10 === true else false
+ */
+
+function divisibleByTen(number) {
+  if (number % 10 === 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(divisibleByTen(10));
+console.log(divisibleByTen(20));
+console.log(divisibleByTen(15));
