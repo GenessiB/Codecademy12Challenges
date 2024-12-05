4c92ab3810468e8bacfa16a063a61c8acae2aaed
Genessi Barot <genessibarot@gmail.com>
2024-12-05 11:07:12 -0500

    problem 3 completed

---
+++ 3.js
@@ -0,0 +1,8 @@
+// 3. Create a length converter function
+// convert kilo to miles 
+const conversion = 1.609; // for miles
+function convert(kilo){
+return kilo / conversion;
+}
+
+console.log(convert(12));
