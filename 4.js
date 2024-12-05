2d265e463fc27b3c01e899571b1a7a91fa749cbc
Genessi Barot <genessibarot@gmail.com>
2024-12-05 12:01:25 -0500

    problem #4 solved (bonus intermediate challenge attempted but not solved)

---
+++ 4.js
@@ -0,0 +1,26 @@
+// 4. Calculate the sum of numbers within an array
+/**  extra challenges:
+ *  create a function that can return the sum of a particular column or row number in a table
+ * make it work with arrays of different lengths and postitive/negative integers
+ */
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [-1, 2, -3, 4, -5];
+const array3 = [1.5, 2.5, 3.5, 4.5, 5.5];
+const array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+function sumOfArray(array) {
+    let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  console.log(sum);
+}
+
+// sumOfArray(array1);
+// sumOfArray(array2);
+//  sumOfArray(array3);
+//  sumOfArray(array4);
+
+
