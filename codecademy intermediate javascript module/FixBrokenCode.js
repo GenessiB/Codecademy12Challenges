a4b92a7c4995386dc56aa98e4e5f5849d5c14a5c
Genessi Barot <genessibarot@gmail.com>
2024-12-07 12:10:55 -0500

    fix broken code will try again later

---
+++ codecademy intermediate javascript module/FixBrokenCode.js
@@ -0,0 +1,9 @@
+// smallestPowerOfTwo(), which should take in an array and return the lowest power of 2 that is greater than or equal to each number in that array. A power of two is any value 2n where n is a non-negative integer (including 0). The list of powers of 2 for non-negative integers starts with [1, 2, 4, 8, 16, 32, 64,...] where 20=1, 21=2, 22=4, and so on.
+
+// const myArray = [6, 2, 11, 50];
+
+// function smallestPowerOfTwo(array){
+// for (let i = 0; i < array.length; i++){
+
+// }
+// }
