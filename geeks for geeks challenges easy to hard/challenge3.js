1c662cd5a3233bf0149632354c5083fa98259462
Genessi Barot <genessibarot@gmail.com>
2024-12-16 13:40:37 -0500

    ch 5

--- geeks for geeks challenges easy to hard/challenge3.js
+++ geeks for geeks challenges easy to hard/challenge3.js
@@ -1,5 +1,5 @@
-// print multiplication table with 7
+// pri.nt multiplication table with 7
 for (let i = 0; i < 13; i++){
 const sum = 7 * i;
 console.log(`7 x ${i} = ${sum}` );
-}
+}   
---
+++ geeks for geeks challenges easy to hard/challenge4.js
@@ -0,0 +1,8 @@
+// print all multiplication tables 1 to 10
+
+for (let i = 1; i < 11; i++){
+    for (let j = 0; j < 11; j++){
+        console.log(`${i} X ${j} = ${i * j}`);
+    }
+}
+
